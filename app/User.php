2700d3508d8d3ab6881390c2a503d967c8d4b339
Tiago Estevao <Tiago.santos@interaktiv.com.br>
2020-06-06 13:01:43 -0300

    Hidden attributes

--- app/User.php
+++ app/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token',"email_verified_at","created_at","updated_at","api_token"
     ];
 
     /**
