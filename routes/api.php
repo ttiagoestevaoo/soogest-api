e210947b682ad32967ce3e3292597f7d8f0fec5d
Tiago Estevao <Tiago.santos@interaktiv.com.br>
2020-06-08 11:14:56 -0300

    Alter routes description

--- routes/api.php
+++ routes/api.php
@@ -14,26 +14,30 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//User login and register
 Route::post('register', 'AuthLoginController@register')->name("api.register");;
 Route::post('login', 'AuthLoginController@login')->name("api.login");;
 
 Route::group(['middleware' => 'auth:api'], function(){
 
-   
+   //Tasks
     Route::get('/tasks','TaskController@index')->name('tasks');
     Route::post('/tasks','TaskController@store');
     Route::get('/tasks/{task}','TaskController@show');
     Route::put('/tasks/{task}','TaskController@update');
     Route::delete('/tasks/{task}','TaskController@destroy');
-    Route::get('details', 'API\UserController@details');
-    Route::post('logout','AuthLoginController@logout')->name("api.logout");
+    
+    //Projetcs
     Route::get('/projects','ProjectController@index')->name('projects');
     Route::post('/projects','ProjectController@store');
     Route::get('/projects/{project}','ProjectController@show');
     Route::put('/projects/{project}','ProjectController@update');
     Route::delete('/projects/{project}','ProjectController@destroy');
+    
+    //User logout and details
     Route::get('/user', 'UserController@show');
+    Route::post('logout','AuthLoginController@logout')->name("api.logout");
+
 
 });
 
